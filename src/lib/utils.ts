b2d9d8294a21b7f65a84e8d623dd717077d645cb
Michael Stone <vector_flys@hotmail.com>
2024-05-24 15:33:12 -0600

    add comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,12 @@
  * Utilities
  */
 
+/**
+ * Function to sleep specified time
+ *
+ * @param ms - number of milliseconds to sleep
+ * @returns
+ */
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
