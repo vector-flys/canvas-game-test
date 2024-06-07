ec30407c6029fc05fcf5e49709a65639de24cd9a
Michael Stone <michael.stone@opscompass.com>
2024-06-07 10:53:36 -0600

    more comments

--- src/gameGrid.ts
+++ src/gameGrid.ts
@@ -19,7 +19,18 @@ import { ShapeGrid } from "./shapeGrid";
 export class GameGrid extends ShapeGrid {
   gridDim: ObjSize;
 
-  // Override draw for the game grid
+  /**
+   * Override draw for the game grid
+   *
+   * @returns void
+   */
+  // draw(): void {
+  //   if (!this.visible) return;
+  //   const spaces = " ".repeat(this.shapeDepth() * 2);
+  //   console.log(
+  //     `${spaces}grid[${this.name}].draw([${this.loc.x}, ${this.loc.y}] ${this.size.w}x${this.size.h})`
+  //   );
+  // }
 
   constructor(
     gridDim: ObjSize,
@@ -27,17 +38,17 @@ export class GameGrid extends ShapeGrid {
     parent?: ShapeNode
   ) {
     super(GameRegion, gridDim, param, parent);
+    // ensure they didn't ask for something stupid
+    if (gridDim.w <= 0 || gridDim.h <= 0) {
+      throw new Error("Grid dimensions must be greater than 0");
+    }
+
+    // Set the grid properties
     this.gridDim = gridDim;
     this.name = "gameGrid";
     this.borderColor = "#40cf40";
     this.divColor = "lightGray";
     this.fillColor = "";
     this.textColor = "white";
-
-    // this.gameRegions = this.shapeGrid as GameRegion[][];
-    // ensure they didn't ask for something stupid
-    if (gridDim.w <= 0 || gridDim.h <= 0) {
-      throw new Error("Grid dimensions must be greater than 0");
-    }
   }
 }
